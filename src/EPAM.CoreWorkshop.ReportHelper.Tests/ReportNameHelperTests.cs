2b124ab9a5f1c7addddfdf62c8bd360b2efc00a1
Denis Baranov <denis_baranov@epam.com>
2018-10-13 14:43:08 +0400

    HOL2. Add runtime-specific code

--- src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using System.Threading;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -7,10 +8,33 @@ namespace EPAM.CoreWorkshop.ReportHelper.Tests
     [TestClass]
     public class ReportNameHelperTests
     {
+        static object[][] NormalizeFileNameTestData
+        {
+            get
+            {
+                object[][] data = new object[][]
+                {
+                    new object[]{"Hello: my world", '_', "Hello_ my world"},
+                    new object[]{"Hello my %world", '_', "Hello my %world"},
+                    new object[]{"Hello my /world", '_', "Hello my _world"}
+                };
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    data[0][2] = "Hello: my world";
+                    data[1][2] = "Hello my %world";
+                    data[2][2] = "Hello my _world";
+                }
+                return data;
+            }
+        }
+
+
         [TestMethod]
         //[DataRow("Hello: my world", '_', "Hello_ my world")]
         [DataRow("Hello my %world", '_', "Hello my %world")]
         [DataRow("Hello my /world", '_', "Hello my _world")]
+        [DynamicData("NormalizeFileNameTestData", DynamicDataSourceType.Property)]
         public void NormalizeFileNameTest(string name, char repl, string expected)
         {
             var result = ReportNameHelper.NormalizeFileName(name, repl);
